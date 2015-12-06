86c2087550bfbaca17dba847913622fc75bf5615
Tim Leerhoff <tleerhof@web.de>
2015-12-06 11:42:07 +0100

    Now works also for items out of main-range.

--- test/tests.js
+++ test/tests.js
@@ -29,6 +29,20 @@ describe('BinTree', function () {
     });
 
     describe('#ForEachInRange()', function () {
+        it('should return zero items when out of range and subdivision level is 0', function () {
+            var resultList = [];
+            var tree = new bintree.BinTree(0, 10, 0);
+            tree.registerItem(new TestItem(11));
+
+            tree.forEachInRange(0, 5, function(item) {
+	      resultList.push(item);
+	    });
+
+            assert.equal(resultList.length, 0);
+        });
+    });
+
+    describe('#ForEachInRange()', function () {
         it('should return one item when in range and subdivision level is 1', function () {
             var resultList = [];
             var tree = new bintree.BinTree(0, 10, 1);
